56fce671a7bec437702c9712462fd271186f2444
LorDClockaN <davor@losinj.com>
2015-06-19 11:07:35 -0600

    Add Restart SystemUI to reboot menu
    Change-Id: Ie00ffd52fec29e47ab8613d6feaf065542b40bd3

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -68,6 +68,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import java.lang.reflect.Method;
+import com.android.internal.util.benzo.Helpers;
 
 public final class ShutdownThread extends Thread {
     // constants
@@ -86,6 +87,7 @@ public final class ShutdownThread extends Thread {
     private static final int MOUNT_SERVICE_STOP_PERCENT = 20;
 
     private static final String SOFT_REBOOT = "soft_reboot";
+    private static final String SYSTEMUI_REBOOT = "systemui_reboot";
 
     // length of vibration before shutting down
     private static final int SHUTDOWN_VIBRATE_MS = 500;
@@ -238,7 +240,12 @@ public final class ShutdownThread extends Thread {
                                             com.android.internal.R.array.shutdown_reboot_actions);
                                     if (selected >= 0 && selected < actions.length) {
                                         mReason = actions[selected];
-                                        if (actions[selected].equals(SOFT_REBOOT)) {
+                                    if (actions[selected].equals(SYSTEMUI_REBOOT)) {
+                                        mRebootReason = actions[selected];
+                                        doSystemUIReboot();
+                                        return;     
+                                   	}
+                                   	if (actions[selected].equals(SOFT_REBOOT)) {
                                             doSoftReboot();
                                             return;
                                         }
@@ -273,6 +280,10 @@ public final class ShutdownThread extends Thread {
         } catch (RemoteException e) {
             Log.e(TAG, "failure trying to perform soft reboot", e);
         }
+    }
+
+    private static void doSystemUIReboot() {
+        Helpers.restartSystemUI();
     }
 
     private static class CloseDialogReceiver extends BroadcastReceiver
